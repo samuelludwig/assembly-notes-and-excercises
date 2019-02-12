9ef581b8a8156c0794f9eb0d3e0f41ee1ab77011
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-12 07:40:33 -0500

    adds additional documentation comments and enhancements

--- calcAreaFP.asm
+++ calcAreaFP.asm
@@ -1,31 +1,36 @@
+# calcAreaFP.asm
+# Calculates the area of a circle, given its radius.
+#
+# Registers used:
+#   $f0     - holds const pi
+#   $f1     - holds radius
+#   $f2     - value of x
+#   $a0     - holds address of area message
+#   $f12    - holds x during float-print syscall
+ 
     .data
-pi:     .float  3.14
-radius: .float  14.17
-fzero:  .float  0.0
-pie:    .asciiz "the pie area is: "
+pi:     .float  3.14    # const pi
+radius: .float  14.17   # radius of circle
+area:    .asciiz "the area is: "
 
     .text
 main:
     l.s     $f0, pi         # load pi constant
     l.s     $f1, radius     # load radius constant
-    la		$t0, pie		# load message address to $t0
 
-    mul.s   $f2, $f0, $f1   # x = pi * r
-    mul.s   $f2, $f2, $f1   # x = x * r
-                            # all together: x = pi*(r^2)
+    mul.s   $f2, $f0, $f1   # x = pi * r    # all together: x = pi*(r^2)
+    mul.s   $f2, $f2, $f1   # x = x * r     #
     
-    la		$a0, pie		# load message
+    la		$a0, area		# load message
     li		$v0, 4		    # print string
     syscall                 #
     
-    l.s     $f4, fzero
-    add.s   $f12, $f2, $f4
-
-    li		$v0, 2		# $v0 = 2
-    syscall
+    mov.s   $f12, $f2       # move x into $f12
+    li		$v0, 2		    # print float in $f12
+    syscall                 #
 
     li		$v0, 10		    # exit
-    syscall
+    syscall                 #
 
 
     
--- floatingPoint.asm
+++ floatingPoint.asm
@@ -1,18 +1,20 @@
-# 
+# floatingPoint.asm 
+# Prints out a floating point value from .data
 #
-#
-#
-#
+# Registers used:
+#   $f12    - holds value of float that is to be printed
+#   $v0     - syscall codes
+
 
     .data
-test1:  .float 3.2
+test1:  .float 3.2      # floating-point number (x)
 
     .text
 main:
-    l.s     $f12, test1 # load 3.2 into $f12
-    li		$v0, 2		# print float
+    l.s     $f12, test1 # load x into $f12
+    li		$v0, 2		# print float in $f12
     syscall             #
 
-    li		$v0, 10		# $v0 = 10
+    li		$v0, 10		# exit
     syscall             #
        
