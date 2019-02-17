7ac55a717c2bcff4950d9bb1d56379b43177c006
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-17 07:46:12 -0500

    fleshes out exp subprogram

--- sinCalc.asm
+++ sinCalc.asm
@@ -89,8 +89,22 @@ sin_calc_done:
 fact:   # args: ($a0 = y)
 # return (y!)
 
-exp:    # args: ($a0 = float(x)), ($a1 = int(exp_val))
-# return (x)^(exp_val)
+exp:    # args: ($f12 = float(x)), ($a1 = int(exp_val))
+        # return (x)^(exp_val)
+    move.s 	$f4, $f12		# $f0 = float(x)
+    li.s    $f5, $f4        # $f1 = total, init to x
+    move 	$t1, $a1		# $t1 = exponent value
+    li		$t2, 0		    # $t2 = exponent counter
+    
+exp_loop:
+    bge		$t2, $t1, exp_loop_done	# if $t2 >= $t1 then exp_loop_done
+    mult.s  $f5, $f5, $f4
+    addi	$t2, $t2, 1		# $t2 = $t2 + 1
+
+exp_loop_done:
+    move 	$f0, $f4		# move total into return register $f0 
+    jr		$ra				# return to $ra
+    
 
 convert_to_radians:     # args: ($a0 = float(x))
 # return (x) * (pi/180)
