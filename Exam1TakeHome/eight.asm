f64dad9a8ed65711a07e518dcc913c5c95e458d8
samuelludwig <samuelludwig74@yahoo.com>
2019-03-12 21:14:02 -0400

    adds to sqrt problem, will test later

--- Exam1TakeHome/eight.asm
+++ Exam1TakeHome/eight.asm
@@ -3,12 +3,12 @@
 # 2. Divide  #
 # 3. Average #
 ##############   
-# G(n+1) = (1/2)(G(n) + (n/G(n))
+# G(n+1) = (1/2)(G(n) + (a/G(n))
    
     .data
 sval:   .float  72673.88
 sg1:    .float  850.0
-s2:     .float  2.0
+s1ovr2: .float  0.5
 
 msg:    .asciiz "\nThe square root of 72673.88 is "
 
@@ -17,9 +17,9 @@ main:
     jal		print_msg	# jump to print_msg and save position to $ra
     nop                 #
 
-    l.s     $f12, sval  # $f12 = 72673.88
+    l.s     $f12, sval  # $f12 = 72673.88 <- a
     l.s     $f14, sg1   # $f13 = initial guess
-    l.s     $f16, s2    # $f16 = 2
+    l.s     $f16, s1ovr2    # $f16 = 0.5 <- (1/2) coefficient
     jal		single_sqrt # jump to single_sqrt and save position to $ra
     nop                 #
 
@@ -30,27 +30,31 @@ print_msg:
     la		$a0, msg	# print contents of msg
     li		$v0, 4		# 
     syscall             #
+    
     jr		$ra         # jump to $ra
 
 
 single_sqrt:
 init_ssqrt:
-    mov.s   $f0, $f12           # $f0 = 72673.88 <- n
-    mov.s   $f10, $f12          # $f10 = n <- x
-    div.s   $f2, $f10, $f16     # $f2 = (n/2) <- first guess <- i
-    sub.s   $f4, $f4, $f4       # $f4 = 0
+    mov.s   $f0, $f12           # $f0 = 72673.88 <- a
+    mov.s   $f1, $f16           # $f1 = (0.5)
+    mov.s   $f10, $f14          # $f10 = 850.0 <- initial guess, G(0) <- old_guess
+    sub.s   $f11, $f11, $f11    # init $f11 to 0 <- will be our new guess
+    
 
 s_sqrt_loop:
-    div.s   $f1, $f0, $f10      # $f1 = n/x
-    add.s   $f3, $f10, $f1      # $f3 = x + (n/x) 
-    div.s   $f8, $f6, $f16      # $f8 = (x + n/x)/2
-    c.lt.s	$f4, $f2	        # if new guess = old guess, then print_s
-    bc1t	s_sqrt_loop			# branch to s_sqrt_loop
+    div.s   $f3, $f0, $f10      # $f3 = (a/old_guess)
+    add.s   $f4, $f10, $f3      # $f4 = (old_guess + (a/old_guess))
+    mul.s   $f11, $f1, $f4      # new_guess = (0.5) * (old_guess + (a/old_guess))
+    
+    c.eq.s	$f10, $f11	        # if new_guess == old_guess, then proceed to print_s
+    bc1f	s_sqrt_loop			# else, branch back to s_sqrt_loop
 
 print_s:
     mov.s   $f12, $f10
     li		$v0, 2		        # print float in $f12
     syscall                     #
+    
     jr		$ra					# jump to $ra
 
 exit:
