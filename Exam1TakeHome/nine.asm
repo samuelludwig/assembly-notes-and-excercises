b7a375124327f19d0122397ae6994f36c9e60cef
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-03-13 06:57:30 -0400

    finishes problem 9

--- Exam1TakeHome/nine.asm
+++ Exam1TakeHome/nine.asm
@@ -15,23 +15,22 @@ plus_loop:
     sw		$t1, 0($t0)	            # arr[i] = (arr[i] + 10)
     addi	$t0, $t0, 4		        # offset += 4 to get to next address 
     addi	$t7, $t7, 1		        # inc counter
-    bgt		$t8, $t7, plus_loop	    # if counter > arr length then plus_done
-    j		plus_done				# else jump to plus_done
-    
-plus_done:
+    bgt		$t8, $t7, plus_loop	    # if counter <= arr length then keep looping
+                                    # else finish loop
+
 add_array:
-    la		$t0, arr		# $t0 = base address of arr
-    li		$t8, 6		    # $t8 = arr length
-    li		$t5, 0		    # $t5 = sum, init to 0
-    li		$t7, 0		    # counter, init to 0
+    la		$t0, arr		    # $t0 = base address of arr
+    li		$t8, 6		        # $t8 = arr length
+    li		$t5, 0		        # $t5 = sum, init to 0
+    li		$t7, 0		        # counter, init to 0
 
 add_loop:
     lw		$t1, 0($t0)		    # $t1 = arr[i] 
     add		$t5, $t5, $t1		# $t5 = $t5 + $t1
     addi	$t0, $t0, 4			# increment base offset by 4
     addi	$t7, $t7, 1			# $t7 = $t7 + 1
-    bgt		$t8, $t7, add_loop	# if counter > arr.len then add_done
-    j		add_done			# jump to add_done
+    bgt		$t8, $t7, add_loop	# if counter > arr.len then proceed to add_done
+                                # else, continue looping
         
 add_done:
 move 	$a0, $t5		# $a0 = $t5
